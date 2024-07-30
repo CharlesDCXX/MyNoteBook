a019640877623828925e9454a6d4b2ae831ca689
duanchenxi <duanchenxicharles@gmail.com>
2024-07-30 23:11:29 +0800

    first leetcode question

---
+++ leetcode/q1.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func twoSum(nums []int, target int) []int {
+	intMap := make(map[int]int)
+	for i, v := range nums {
+		intMap[v] = i
+	}
+	for i, v := range nums {
+		vTemp := target - v
+		if j, ok := intMap[vTemp]; ok {
+			if i != j {
+				return []int{i, j}
+			}
+
+		}
+	}
+	return []int{}
+}
+
+// 第一题，刷很多遍了，算重新开始，睡觉吧
