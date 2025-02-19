adaa2d04ff3122b934b688623161f8ab0f5c128f
haiquePC <duanchenxicharles@gmail.com>
2025-02-19 10:59:28 +0800

    add

---
+++ leetcode/20250219_q27.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+
+}
+
+func removeElement(nums []int, val int) int {
+	var l int
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[l] = nums[i]
+			l++
+		}
+	}
+	return l
+}
